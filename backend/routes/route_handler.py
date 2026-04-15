684e2c721fc52a74ac72bfafd288d6f82ae619d2
Shivam-Narayan <sshivam6495@gmail.com>
2026-04-15 23:10:08 +0530

    Comment out intent router inclusion in route handler

--- backend/routes/route_handler.py
+++ backend/routes/route_handler.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from routes.intent_routes import intent_router
+# from routes.intent_routes import intent_router
 
 
 def all_routers(app: FastAPI):
     """Include all routers in the FastAPI app."""
-    app.include_router(intent_router)
+    # app.include_router(intent_router)
